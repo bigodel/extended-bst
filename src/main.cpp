67e3a1f1bcbf92dde24bd359d14e3a6fa0751134
João Pedro <jpedrodeamorim@gmail.com>
2018-10-25 17:18:39 -0300

    Fixed merge conflicts with 'nthElement'

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,6 @@ int main(/*int argc, char *argv[]*/)
 {
     BST tree;
 
-<<<<<<< HEAD
-    tree.insert(100);
-    tree.insert(150);
-    tree.insert(40);
-    tree.insert(35);
-    tree.insert(50);
-    tree.insert(120);
-    tree.insert(170);
-    tree.insert(25);
-    tree.insert(45);
-    tree.insert(130);
-    tree.insert(25);
-    tree.insert(25);
-
-    std::cout << tree.root->nLeft << "\n";
-||||||| merged common ancestors
-    tree.insert(10);
-    tree.insert(20);
-=======
     tree.insert(100);
     tree.insert(40);
     tree.insert(150);
@@ -33,17 +14,11 @@ int main(/*int argc, char *argv[]*/)
     tree.insert(25);
     tree.insert(45);
     tree.insert(130);
->>>>>>> nthElement
 
     std::cout << tree.toString() << std::endl;
-<<<<<<< HEAD
-
-||||||| merged common ancestors
-=======
 
     for (int i = 1; i < 11; ++i)
         std::cout << tree.nthElement(i) << "\n";
 
->>>>>>> nthElement
     return 0;
 }
